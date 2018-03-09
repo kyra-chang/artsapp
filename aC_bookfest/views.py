2eec7bfadde0c5822d34fe48e65768bf7c50ad98
Kyra Chang <kyra_chang@berkeley.edu>
2018-03-08 17:01:55 -0800

    change file directory and getting updated to AWS

--- aC_bookfest/views.py
+++ aC_bookfest/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+# def model_form_upload(request):
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = DocumentForm()
+#     return render(request, 'form/simple_upload.html', {
+#         'form': form
+#     })
+
+def post_list(request):
+    return render(request, 'alpha/index.html', {})
--- artistiCal/settings.py
+++ artistiCal/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -104,7 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.MemoryFileUploadHandler",
+ "django.core.files.uploadhandler.TemporaryFileUploadHandler"]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
